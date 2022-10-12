e1f7dd1d7ea73e78f90a6ac364fb2fbc7bb67825
Atanas Patterson - Ianev <ampattersonianeviane@kudlick29.cs.usfca.edu>
2022-10-11 17:05:45 -0700

    project03 files

--- project03.c
+++ project03.c
@@ -1,44 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int minimax (char *board [3][3], int row, int column, int depth) {
-	int isWinnwer = findWinner(char *board [3][3]);
-	if(isWinner){
-		
-	}	
-			/*	base cases:
-					if maximizer wins
-						score = max;
-					if minimizer wins
-						socre = min
-					if draw (there are no more free spaces)
-						scorew = 0
-				recursive case
-					for rows r:
-						for columns c:
-							is space is open
-								recursive(r, c) decomposes the data by filling that space up
-
-			the way scoring goes is like a tree, when we want to calculate the best move we consider all possible cases. for example, player chooses top corner. One of the cases is ai chooses
-			middle, and x may go bottom right after that. we reach these cases with recursion. With every possible board, we go to a case where someone wins or draws.
-
-			The program is: make a move, switch players, recurse, print computer move and so on until winner or a draw.
-
-			Minimax is exhaustive; all moves are scored MINIMAX: recursively finds a score for all possible moves amoing thousands of choices
-
-			example of maximizer (minimizer would be the opposite, less is better)
-				1 iteration that leads to a win = + 2
-				1 iteratio "" = +7 this becomes the new best move
-			sooner wins are better, and to know this we find the depth of the recursion. A way to do this is to call minimax at first with depth 0 and when we recurse we call minimax*/ with depth +1.
-
-	return ();
-}
-
-void printBoard (char board[3][3]) {
+// function that prints out the current state of the board
+void printBoard (char board[3][3]) {                                                        
 	for (int row = 0; row < 3; row++) {
 		for (int column = 0; column< 3; column++) {
 			if (column == 0 || column == 1)
 				printf(" %c |", board[row][column]);
-			else if (column == 2){
+			else if (column == 2) {
 				printf(" %c", board[row][column]);
 			}
 		}
@@ -48,135 +17,201 @@ void printBoard (char board[3][3]) {
 	}
 }
 
-int findWinner (char board[3][3]) {
-	for (int i = 0; i < 3; i++) {
-		if ((board[i][0] == board[i][1]) && (board[i][1] == board[i][2])) {
-			if (board[i][0] == 'X') {
-				printf("X wins\n");
-				return 1;	
+// function that returns an integer depending on the state of the board (X win, O win, draw or no win)
+int findWinner (char board[3][3]) {																
+	for (int row = 0; row < 3; row++) {																			// case of horizontal win
+		if ((board[row][0] == board[row][1]) && (board[row][1] == board[row][2])) {
+			if (board[row][0] == 'O') {
+				return 10;	
 			}
-			else if (board[i][0] == 'O') {
-				printf("O wins\n");
-				return -1;			
+			else if (board[row][0] == 'X') {
+				return -10;			
+			}
+		}
+	}
+	
+	for (int column = 0; column < 3; column++) {																// case of vertical win
+		if ((board[0][column] == board[1][column]) && (board[1][column] == board[2][column])) {
+			if (board[0][column] == 'O') {
+				return 10;	
+			}
+			else if (board[0][column] == 'X') {
+				return -10;			
 			}
 		}
 	}
 
-	for (int i = 0; i < 3; i++) {
-		if ((board[0][i] == board[1][i]) && (board[1][i] == board[2][i])) {
-			if (board[0][i] == 'X') {
-				printf("X wins\n");
-				return 1;	
-			}
-			else if (board[0][i] == 'O') {
-				printf("O wins\n");
-				return -1;			
-			}
+	if ((board[0][0] == board[1][1]) && (board[1][1] == board[2][2])) {											// case of diagonal win
+		if (board[0][0] == 'O') {
+			return 10;	
 		}
-	}
-
-	if ((board[0][0] == board[1][1]) && (board[1][1] == board[2][2])) {
-		if (board[0][0] == 'X') {
-			printf("X wins\n");
-			return 1;	
-		}
-		else if (board[0][0] == 'O') {
-			printf("O wins\n");
-			return -1;			
+		else if (board[0][0] == 'X') {
+			return -10;			
 		}
 	} else if ((board[0][2] == board[1][1]) && (board[1][1] == board[2][0])) {
-		if (board[0][2] == 'X') {
-			printf("X wins\n");
-			return 1;	
+		if (board[0][2] == 'O') {
+			return 10;	
 		}
-		else if (board[0][2] == 'O') {
-			printf("O wins\n");
-			return -1;			
+		else if (board[0][2] == 'X') {
+			return -10;			
 		}
 	}
-
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
-			if (board[i][j] == '_'){
-				return -2;
+	
+	for (int row = 0; row < 3; row++) {																			// case of no winner (still unmarked spaces)
+		for (int column = 0; column < 3; column++) {
+			if (board[row][column] == '_'){
+				return -1;
 			}
 
-		}
+		}	
 	}
-
-	printf("draw\n");
-	return 0;
+	return 0;																									// case of draw
 }
 
-int * findBestMove (char *board[3][3]) {
-	int bestScore = -100;
-	int bestSquare[2];
+// function that returns the score for a move
+int minimax (char board [3][3], char player, int depth) {
+	int score;
+	int bestScore;
+		
+	int isWinner = findWinner(board);
+	if (isWinner == 10)
+		return isWinner - depth;
+	else if (isWinner == -10)
+		return isWinner + depth;
+	else if (isWinner == 0)
+		return isWinner;
+
+	if (player == 'O') {
+		bestScore = -100;
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				if (board[i][j] == '_') {
+					board[i][j] = 'O';
+					score = minimax(board, 'X', depth + 1);
+					if (score > bestScore)
+						bestScore = score;
+					board[i][j] = '_';	
+				}
+			}
+		}
+	} else if (player == 'X') {
+		bestScore = 100;
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				if (board[i][j] == '_') {
+					board[i][j] = 'X';
+					score = minimax(board, 'O', depth + 1);
+					if (score < bestScore)
+						bestScore = score;
+					board[i][j] = '_';	
+				}
+			}
+		}
+	}
+	return bestScore;
+}
+
+// function that assigns the row and column variables with the best possible move
+void findBestMove (char board[3][3], char player, int *r, int *c ) {
 	int currScore;
+	int bestScore;
+
+	if (player == 'X')
+		bestScore = 100;
+	else
+		bestScore = -100;
+
 	for (int row = 0; row < 3; row++) {
 		for (int column = 0; column < 3; column++) {
-			if(*board[row][column] == '_'){
-				currScore = minimax(board, row, column, 0);
-				if (currScore > bestScore){
-					bestScore = currScore;
-					bestSquare[0] = row;
-					bestSquare[1] = column;
+			if (board[row][column] == '_') {
+				board[row][column] = player;
+				if (player == 'X') {
+					currScore = minimax(board, 'O', 1);
+					if (currScore < bestScore) {
+						bestScore = currScore;
+						*r = row;
+						*c = column;
+					}
+					board[row][column] = '_';
+				} else {
+					currScore = minimax(board, 'X', 1);
+					if (currScore > bestScore) {
+						bestScore = currScore;
+						*r = row;
+						*c = column;
+					}
+					board[row][column] = '_';
 				}
 			}
 		}	
 	}
-	return bestSquare;
 }
 
+// function that prints out the result of a ttt match
+void printResult (int result) {
+	if (result == 10)
+		printf("O wins\n");
+	else if (result == -10)
+		printf("X wins\n");
+	else if (result == 0)
+		printf("draw\n");
+}
 
-
-
+// function that initializes the ttt board
+void initBoard(char board[3][3], int value, char *argv[]) {
+	int k = 1;
+	if (value > 1) {
+		for (int row = 0; row < 3; row++) {
+			for (int column = 0; column < 3; column++) {
+				board[row][column] = *argv[k];
+				k++;
+			}
+		}
+	} else {
+		for (int row = 0; row < 3; row++) {
+			for (int column = 0; column < 3; column++) {
+				board[row][column] = '_';
+			}
+		}
+	}
+}
 
 int main (int argc, char *argv[]) {
+	char board[3][3];
+	initBoard(board, argc, argv);
+	int r;
+	int c;
 
+	if (argc > 1) {																				// case if player wants to know the next best move
+		printBoard(board);
+		findBestMove(board,'O', &r, &c);
+		printf("O: %d %d\n", r, c);
+	} else {																					// case if player wants to play against computer
+		int gameDone = -1;
+		printBoard(board);
+		while (gameDone == -1) {
+			printf("X: ");
+			fscanf(stdin, "%d %d", &r, &c);
+			if ((board[r][c] == '_') && (r >= 0) && (r < 3) && (c >= 0) && (c < 3)) {			// check if player move is valid
+				board[r][c] = 'X';
+				if (gameDone != -1)
+					break;
+				findBestMove(board, 'O', &r, &c);
+				board[r][c] = 'O';
+				printf("O moves to %d %d\n", r, c);
+				printBoard(board);
+				gameDone = findWinner(board);
+			} else {
+				printf("invalid move\n");
+				break;
+			}
+		}		
 
-		/* how to do minimax
-
-				base cases:
-					if maximizer wins
-						score = max;
-					if minimizer wins
-						socre = min
-					if draw (there are no more free spaces)
-						scorew = 0
-				recursive case
-					for rows r:
-						for columns c:
-							is space is open
-								recursive(r, c) decomposes the data by filling that space up
-
-			the way scoring goes is like a tree, when we want to calculate the best move we consider all possible cases. for example, player chooses top corner. One of the cases is ai chooses
-			middle, and x may go bottom right after that. we reach these cases with recursion. With every possible board, we go to a case where someone wins or draws.
-
-			The program is: make a move, switch players, recurse, print computer move and so on until winner or a draw.
-
-			Minimax is exhaustive; all moves are scored MINIMAX: recursively finds a score for all possible moves amoing thousands of choices
-
-			example of maximizer (minimizer would be the opposite, less is better)
-				1 iteration that leads to a win = + 2
-				1 iteratio "" = +7 this becomes the new best move
-			sooner wins are better, and to know this we find the depth of the recursion. A way to do this is to call minimax at first with depth 0 and when we recurse we call minimax with depth +1.
-
-			need a function that records scores and picks the best one:
-
-				findbestmove(board)
-					runningbestscore (if youre minimizer lowest score, if maximizer highest score)
-					loop over board
-						score = minimax(r, c)
-						if score is better
-							runningbestscore = score
-							keep r and c that generated that score
-						else
-							undo that move
-							try again
-					move to best r, c
-			
-
-
+		if (gameDone != -1){
+			printBoard(board);
+			printResult(gameDone);
+		}
+	}
 	
-	*/return 0;
+	return 0;
 }
